2e1164ed03d199c6fe98c63568c1476a14d3b9ce
Hugo Eseverri <hugooeseverri@gmail.com>
2025-04-07 10:26:38 -0300

    creación de componentes

---
+++ mini-notas/src/components/Home/Características/Características.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Características() {
+    return (
+        <div>
+            <h1 className="text-8xl text-black py-[50px]">Características</h1>
+        </div>
+    )
+}
+
+export default Características
