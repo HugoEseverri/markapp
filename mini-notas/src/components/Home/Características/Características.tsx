1289af107f24a3ff27ca1f7e731fbdc9bf9f0ad6
Hugo Eseverri <hugooeseverri@gmail.com>
2025-04-08 10:11:47 -0300

    se modifican estilos en componente nuevo

--- mini-notas/src/components/Home/Características/Características.tsx
+++ mini-notas/src/components/Home/Características/Características.tsx
@@ -3,9 +3,27 @@ import React from 'react'
 function Características() {
     return (
         <div>
-            <h1 className="text-8xl text-black py-[50px]">Características</h1>
-        </div>
-    )
+            <div className='text-left'>
+                <h1 className="text-8xl text-black py-[50px]">¿Preguntas Frecuentes?</h1>
+                <h2 className='text-6xl text-black '>¡Respuestas rápidas!</h2>
+                <p>Encuentra respuestas a las preguntas más comunes sobre el uso de Mark App y sacá el máximo provecho de nuestra apliación.</p>
+            </div>
+            <div className='text-center'>
+                <div>
+                    <h3>Problemas Técnicos</h3>
+                    <p>Si experimentas algún problema técnico con Mark App, nuestro equipo de soporte está acá para ayudarte. Contactanos para recibir asistencia inmediata y resolver cualquier inconveniente que puedas enfrentar.</p>
+                /div>
+                div>
+                    <h3>Compatibilidad de Dispositivos</h3>
+                    <p>Mark App es compatible con una amplia gama de dispositivos, incluyendo computadoras, teléfonos y tabletas. Asegúrate de tener la última versión de la aplicación para disfrutar de todas las funciones en tu dispositivo preferido.</p>
+                /div>
+                div>
+                    <h3>Actualizaciones de funciones</h3>
+                    <p>Mantente al día con las últimas actualizaciones y nuevas funciones de Mark App. Nuestro equipo trabaja constantemente para mejorar la aplicación y brindarte una experiencia aún mejor en la gestión de tus notas.</p>
+                /div>
+                </div>
+            </div>
+            )
 }
 
-export default Características
+            export default Características
